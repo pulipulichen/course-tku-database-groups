7ccd4ccdd270f46d68cbd8d9da708edf0c7735fd
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-10-21 23:08:09 +0800

    西元2023年10月21日 (週六) 23時08分09秒 CST

--- grouping/terminal.sh
+++ grouping/terminal.sh
@@ -5,4 +5,4 @@ cd "$(dirname "$0")"
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 
-gnome-terminal -- bash -c "$script_dir/main.sh; read -n 1 -s -r -p \"Press any key to continue...\"; exec bash"
+gnome-terminal -- bash -c "~/course-tku-database-groups/grouping/main.sh; read -n 1 -s -r -p \"Press any key to continue...\"; exec bash"
